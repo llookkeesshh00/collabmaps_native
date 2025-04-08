6fa33716d7db329cb42d5399520826d770523201
llookkeesshh00 <llookkeesshh00@gmail.com>
2025-04-08 21:01:40 +0530

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="bg-blue-500 p-4">
+    <Text className="text-white">Testing NativeWind</Text>
+  </View>
+  );
+}
