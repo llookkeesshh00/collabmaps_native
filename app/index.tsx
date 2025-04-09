af5423ee95c014351cb438a4578568657e06613e
esannihith <esanni04@gmail.com>
2025-04-09 08:29:32 +0530

    hi there

--- app/index.tsx
+++ app/index.tsx
@@ -3,7 +3,7 @@ import { Text, View } from "react-native";
 export default function Index() {
   return (
     <View className="bg-blue-500 p-4">
-    <Text className="text-white">Testing NativeWind</Text>
+    <Text className="text-white">samml change</Text>
   </View>
   );
 }
